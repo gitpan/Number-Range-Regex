ec280638b9a2ece07dfea3f4f0479e0596473f52
Brian Szymanski <skibrianski+cpan@gmail.com>
2012-08-23 15:34:28 +0000

    Import of BRIANSKI/Number-Range-Regex-0.01 from CPAN.
    gitpan-cpan-distribution: Number-Range-Regex
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         BRIANSKI/Number-Range-Regex-0.01.tgz
    gitpan-cpan-author:       BRIANSKI
    gitpan-cpan-maturity:     released

---
+++ lib/Number/Range/Regex.pm
@@ -0,0 +1,256 @@
+# Number::Range::Regex
+#
+# Copyright 2012 Brian Szymanski.  All rights reserved.  This module is
+# free software; you can redistribute it and/or modify it under the same
+# terms as Perl itself.
+
+package Number::Range::Regex;
+
+use strict;
+use vars qw (@ISA @EXPORT @EXPORT_OK $VERSION ); 
+eval { require warnings; }; #it's ok if we can't load warnings
+
+require Exporter;
+use base 'Exporter';
+@ISA    = qw( Exporter );
+@EXPORT = @EXPORT_OK = qw( regex_range );
+
+$VERSION = '0.01';
+
+my $_init_opts = {};
+
+sub features {
+  return { negative => 0 };
+}
+
+sub init {
+  my ($self, %opts) = @_;
+  $_init_opts = { %opts };
+}
+
+# TODO: support for auto-swapping of min, max if necessary via an option (instead of die'ing)
+
+# regex_range( $min, $max ); #undef = no limit, so. e.g.
+#   regex_range(3, undef) yields the equivalent of qr/[+]?[3-9]|\d+/;
+sub regex_range {
+  my ($min, $max, $passed_opts) = @_;
+
+  # local options can override defaults from init()
+  my $opts;
+  if($passed_opts) {
+    die "regex_range: too many arguments" unless ref $passed_opts eq 'HASH';
+    $opts = { %$_init_opts };
+    while (my ($key, $val) = each %$passed_opts) {
+      $opts->{$key} = $val;
+    }
+  } else {
+    $opts = $_init_opts;
+  }
+
+  #canonicalize min and max by removing leading zeroes unless the value is 0
+  if(defined $min) { $min =~ s/^0+//; $min = 0 if $min eq ''; }
+  if(defined $max) { $max =~ s/^0+//; $max = 0 if $max eq ''; }
+
+  # regex_range() => error by default
+  if( $opts->{allow_wildcard} ) {
+    if(!defined $min and !defined $max) {
+      return qr/[+]?\d+/;
+      #return qr/[-+]?\d+/;
+    }
+  } else {
+    die "regex_range: must specify either a min, a max, or the allow_wildcard option" unless defined $min or defined $max;
+  }
+
+  if(defined $min) {
+    if( $min !~ /^[+]?\d+$/ ) {
+    #if( $min !~ /^[-+]?\d+$/ ) {
+      die "regex_range: min ($min) must be a positive number or undef";
+    }
+  }
+  if(defined $max) {
+    if( $max !~ /^[+]?\d+$/ ) {
+#    if( $max !~ /^[-+]?\d+$/ ) {
+      die "regex_range: max ($max) must be a positive number or undef";
+    }
+  }
+
+  my $goto_positive_infinity = 0;
+  if( !defined $min ) {
+    $min = 0;
+    #$goto_negative_infinity = 1;
+  }
+  if( !defined $max ) {
+    die "min < 0 unsupported when max undefined" if($min < 0);
+
+    # iterate from $min to the next (power of 10) - 1 (e.g. 9999)
+    # then spit out a regex for any integer with a longer length
+    $max = "9"x length $min;
+
+    $goto_positive_infinity = 1;
+  }
+  if($opts->{autoswap} && ($min > $max)) {
+    ($min, $max) = ($max, $min);
+  }
+
+  return qr/0*$min/ if $min == $max;
+
+  die "regex_range; min must be less than or equal to max" if $max < $min;
+
+  die "TODO: support for negative values not yet implemented" if $min < 0;
+
+#  $min-- unless $opts->{exclusive_min} || $opts->{exclusive};
+#  $max++ unless $opts->{exclusive_max} || $opts->{exclusive};
+#  warn "WARNING: exclusive ranges untested!" if($opts->{exclusive_min} || $opts->{exclusive_max} || $opts->{exclusive});
+
+  my $ndigits = length($max);
+  my $padded_min = sprintf("%0${ndigits}d", $min);
+
+  my $samedigits = 0;
+  for my $digit (0..length($max)-1) {
+    last unless substr($padded_min, $digit, 1) eq substr($max, $digit, 1);
+    $samedigits++;
+  }
+
+  my ($rightmost, $leftmost) = (length $max, $samedigits+1);
+
+#warn "min: $min, max: $max, samedigits: $samedigits, rightmost: $rightmost, leftmost: $leftmost";
+
+  my @patterns = ();
+
+  my $header = substr($min, 0, $rightmost);
+  for(my $digit_pos = $rightmost; $digit_pos >= $leftmost; $digit_pos--) {
+    $header = substr($header, 0, length($header)-1);
+    my $trailer_len = $rightmost - $digit_pos;
+    my $trailer = '\d'x$trailer_len;
+
+    my $digit   = substr($padded_min, $digit_pos-1, 1);
+
+    my $digit_min = $digit+1;
+    $digit_min-- if $digit_pos == $rightmost; #inclusive in ones column only!
+    my $digit_max = $max - ($header.('0'x($trailer_len+1)));
+    $digit_max = int($digit_max / (10**$trailer_len));
+    $digit_max-- if $trailer_len; # inclusive only when this is the last
+    $digit_max = 9 if $digit_max > 9;
+#warn "digit_min: $digit_min, digit_max: $digit_max, header: $header, padded_header: ".($header.('0'x($trailer_len+1)))."\n";
+
+    my $digit_range;
+    if($digit_max < $digit_min) {
+      #warn "empty digit_range, skipping: min: $digit_min, max: $digit_max";
+      next;
+    } elsif($digit_max == $digit_min) {
+      $digit_range = $digit_min;
+    } else {
+      $digit_range = "[$digit_min-$digit_max]";
+    }
+    push @patterns, $header.$digit_range.$trailer;
+#warn "digit_pos: $digit_pos, header: $header, digit: $digit, header: $header, digit_range: $digit_range, trailer: $trailer\n";
+  }
+#warn "leaving top loop";
+
+  for(my $digit_pos = $leftmost+1; $digit_pos <= $rightmost; $digit_pos++) {
+    my $header = substr($max, 0, $digit_pos-1);
+    my $trailer_len = $rightmost - $digit_pos;
+    my $trailer = '\d'x$trailer_len;
+
+    my $digit   = substr($max, $digit_pos-1, 1);
+
+    my $digit_min = 0;
+    my $digit_max = $digit;
+    $digit_max-- if $trailer_len; # inclusive only when this is the last
+#warn "digit_min: $digit_min, digit_max: $digit_max, header: $header, padded_header: ".($header.('0'x($trailer_len+1)))."\n";
+
+    my $digit_range;
+    if($digit_max < $digit_min) {
+      #warn "empty digit_range, skipping: min: $digit_min, max: $digit_max";
+      next;
+    } elsif($digit_max == $digit_min) {
+      $digit_range = $digit_min;
+    } else {
+      $digit_range = "[$digit_min-$digit_max]";
+    }
+    push @patterns, $header.$digit_range.$trailer;
+#warn "digit_pos: $digit_pos, header: $header, digit: $digit, header: $header, digit_range: $digit_range, trailer: $trailer\n";
+  }
+  if( $goto_positive_infinity ) {
+    my $min_digits = length($max)+1;
+    push @patterns, "\\d{$min_digits,}";
+  }
+
+  my $regex_str = join '|', @patterns;
+  return qr/0*(?:$regex_str)/;
+
+}
+
+1;
+
+# TODO: do we want an oop version? seems kinda pointless considering
+# that we are only generating a regex/string...
+
+__END__
+
+=head1 NAME
+
+Number::Range::Regex - create regular expressions that check for
+                       integers in a given range
+
+=head1 SYNOPSIS
+
+  use Number::Range::Regex;
+  my $range = regex_range( 15, 3210 );
+  if( $jibberish =~ /$range/ ) {
+    print "your jibberish contains an integer between 15 and 3210";
+  }
+  if( $num =~ /^$range$/ ) {
+    print "$num is an integer between 15 and 3210";
+  }
+  if( $line =~ /^\S+\s+$range\s/ ) {
+    print "the second field is an integer between 15 and 3210";
+  }
+  my $octet = regex_range(0, 255);
+  my $ip4_match = qr/^$octet\.$octet\.$octet\.$octet$/;
+  my $range_96_to_127 = regex_range(96, 127);
+  my $my_slash26_match = qr/^192\.168\.42\.$range_96_to_127$/;
+  my $my_slash19_match = qr/^192\.168\.$range_96_to_127\.$octet$/;
+  
+
+=head1 DESCRIPTION
+
+which is more legible - this?
+
+  $date =~ m/^0*(?:[1-9]|[12][0-9]|3[01])\/0*(?:[0-9]|1[012])$/;
+
+or this?
+
+  my $day_range = regex_range(1, 31);
+  my $month_range = regex_range(1, 12);
+  $date =~ m/^$day_range\/$month_range$/;
+
+(bonus points if you spotted the bug)
+
+
+=head1 NOTES
+
+It's usually better to check for number-ness only in the regular
+expression and verify the range of the number separately, eg:
+  $line =~ /^\S+\s+(\d+)/ && $1 > 15 && $1 < 32;
+but it's not always practical to refactor in that way.
+
+If you like one-liners, something like the following may suit you...
+  m{^${\( regex_range(1, 31) )}\/${\( regex_range(1, 12) )}$}
+but i certainly don't recommend it!
+
+=head1 NOTES
+
+Non-negative integers only for now.
+
+=head1 AUTHOR
+
+Brian Szymanski  B<< <ski-cpan@allafrica.com> >> -- be sure to put
+Number::Range::Regex in the subject line if you want me to read
+your message.
+
+=head1 SEE ALSO
+
+perl(1), etc.
+
+=cut
---
+++ t/02-usage.t
@@ -0,0 +1,256 @@
+#!perl -w
+$|++;
+
+use strict;
+use Test::More tests => 1395;
+use lib "./blib/lib";
+
+use Number::Range::Regex qw ( regex_range );
+my $features = Number::Range::Regex::features();
+my $range;
+
+Number::Range::Regex->init( foo => "bar" );
+ok(1); #called init() without dying
+eval {
+  Number::Range::Regex->regex_range( 3, 4 );
+}; ok($@);
+eval { regex_range( "three", 4 ); }; ok($@);
+eval { regex_range( 0, "four" ); }; ok($@);
+Number::Range::Regex->init();
+ok(1); #called init() without dying (again)
+
+# min must not be less than max unless autoswap option is set
+$range = eval { regex_range( 12, 8 ) }; ok($@); # min > max
+$range = eval { regex_range( 12, 8, { autoswap => 1 } ) }; ok(!$@);
+ok( $range );
+ok(7 !~ /^$range$/);
+ok(8 =~ /^$range$/);
+ok(9 =~ /^$range$/);
+ok(10 =~ /^$range$/);
+ok(11 =~ /^$range$/);
+ok(12 =~ /^$range$/);
+ok(13 !~ /^$range$/);
+
+# tests for regex_range(undef, undef) aka "wildcarding"
+eval { regex_range() }; ok($@); # must specify at least a min or a max
+my $wildcard_range = eval { regex_range( undef, undef, { allow_wildcard => 1 } ) };
+ok(!$@);
+ok($wildcard_range);
+ok( $features->{negative} ? "-90" =~ /^$wildcard_range$/ : 1 );
+ok( "67" =~ /^$wildcard_range$/ );
+ok( "+67" =~ /^$wildcard_range$/ );
+ok( "0" =~ /^$wildcard_range$/ );
+ok( ".3" !~ /^$wildcard_range$/ ); #no decimal support
+
+my $equals_four = regex_range( 4, 4 );
+ok($equals_four);
+ok("3" !~ /^$equals_four$/);
+ok("4" =~ /^$equals_four$/);
+ok("4.1" !~ /^$equals_four$/);
+ok("40" !~ /^$equals_four$/);
+ok("5" !~ /^$equals_four$/);
+
+my $equals_one = regex_range( 1, 1 );
+ok($equals_one);
+ok("0" !~ /^$equals_one$/);
+ok("1" =~ /^$equals_one$/);
+ok("1.1" !~ /^$equals_one$/);
+ok("10" !~ /^$equals_one$/);
+ok("2" !~ /^$equals_one$/);
+
+my $equals_zero = regex_range( 0, 0 );
+ok($equals_zero);
+ok("-1" !~ /^$equals_zero$/);
+ok("0" =~ /^$equals_zero$/);
+ok("0.1" !~ /^$equals_zero$/);
+ok("00" =~ /^$equals_zero$/); # leading zeroes ok for zero!
+ok("1" !~ /^$equals_zero$/);
+
+my $four_or_five = regex_range( 4, 5 );
+ok($four_or_five);
+ok("3" !~ /^$four_or_five$/);
+ok("4" =~ /^$four_or_five$/);
+ok("4.1" !~ /^$four_or_five$/);
+ok("40" !~ /^$four_or_five$/);
+ok("5" =~ /^$four_or_five$/);
+ok("6" !~ /^$four_or_five$/);
+
+my $nine_or_ten = regex_range( 9, 10 );
+ok($nine_or_ten);
+ok("8" !~ /^$nine_or_ten$/);
+ok("9" =~ /^$nine_or_ten$/);
+ok("9.1" !~ /^$nine_or_ten$/);
+ok("90" !~ /^$nine_or_ten$/);
+ok("10" =~ /^$nine_or_ten$/);
+ok("11" !~ /^$nine_or_ten$/);
+
+my $zero_or_more = regex_range( 0, undef );
+ok($zero_or_more);
+ok("0" =~ /^$zero_or_more$/);
+ok("1" =~ /^$zero_or_more$/);
+ok("1.5" !~ /^$zero_or_more$/);
+ok("99" =~ /^$zero_or_more$/);
+
+my $zero_or_less = regex_range( undef, 0 );
+ok($zero_or_less);
+ok("0" =~ /^$zero_or_less$/);
+ok("1" !~ /^$zero_or_less$/);
+
+my $one_or_more = regex_range( 1, undef );
+ok($one_or_more);
+ok("0" !~ /^$one_or_more$/);
+ok("1" =~ /^$one_or_more$/);
+ok("1.5" !~ /^$zero_or_more$/);
+ok("99" =~ /^$one_or_more$/);
+
+my $one_or_less = regex_range( undef, 1 );
+ok($one_or_less);
+ok("0" =~ /^$one_or_less$/);
+ok("1" =~ /^$one_or_less$/);
+ok("2" !~ /^$one_or_less$/);
+
+my $nine_ninety_seven_or_more = regex_range( 997, undef );
+ok($nine_ninety_seven_or_more);
+ok("0" !~ /^$nine_ninety_seven_or_more$/);
+ok("9" !~ /^$nine_ninety_seven_or_more$/);
+ok("99" !~ /^$nine_ninety_seven_or_more$/);
+ok("996" !~ /^$nine_ninety_seven_or_more$/);
+ok("997" =~ /^$nine_ninety_seven_or_more$/);
+ok("998" =~ /^$nine_ninety_seven_or_more$/);
+ok("1000" =~ /^$nine_ninety_seven_or_more$/);
+ok("23456" =~ /^$nine_ninety_seven_or_more$/);
+
+my $nine_ninety_seven_or_less = regex_range( undef, 997 );
+ok($nine_ninety_seven_or_less);
+ok("0" =~ /^$nine_ninety_seven_or_less$/);
+ok("9" =~ /^$nine_ninety_seven_or_less$/);
+ok("99" =~ /^$nine_ninety_seven_or_less$/);
+ok("996" =~ /^$nine_ninety_seven_or_less$/);
+ok("997" =~ /^$nine_ninety_seven_or_less$/);
+ok("998" !~ /^$nine_ninety_seven_or_less$/);
+ok("1000" !~ /^$nine_ninety_seven_or_less$/);
+ok("23456" !~ /^$nine_ninety_seven_or_less$/);
+
+
+$range = test_range_exhaustive(19825, 20120);
+
+$range = test_range_partial(19825, 32101, [19800, 19911]);
+$range = test_range_partial(19825, 32101, [31990, 32200]);
+$range = test_range_random(19825, 32101, 100);
+ok(0 !~ /^$range$/);
+ok(1982 !~ /^$range$/);
+ok(2000 !~ /^$range$/);
+ok(3000 !~ /^$range$/);
+ok(25000 =~ /^$range$/);
+
+$range = test_range_random(354, 13123, 100);
+ok(0 !~ /^$range$/);
+ok(3 !~ /^$range$/);
+ok(35 !~ /^$range$/);
+ok(354 =~ /^$range$/);
+ok(355 =~ /^$range$/);
+ok(1000 =~ /^$range$/);
+ok(2000 =~ /^$range$/);
+ok(3000 =~ /^$range$/);
+ok(4000 =~ /^$range$/);
+ok(5000 =~ /^$range$/);
+ok(6000 =~ /^$range$/);
+ok(7000 =~ /^$range$/);
+ok(8000 =~ /^$range$/);
+ok(9000 =~ /^$range$/);
+ok(10000 =~ /^$range$/);
+ok(11000 =~ /^$range$/);
+ok(12000 =~ /^$range$/);
+ok(13000 =~ /^$range$/);
+ok(13100 =~ /^$range$/);
+ok(13120 =~ /^$range$/);
+ok(13123 =~ /^$range$/);
+ok(131234 !~ /^$range$/);
+
+test_range_exhaustive(123, 129);
+test_range_exhaustive(103, 129);
+test_range_exhaustive(1234, 1239);
+test_range_exhaustive(1229, 1239);
+test_range_exhaustive(1129, 1239);
+
+# leading zero tests
+$range = test_range_exhaustive("07", 128);
+ok(6 !~ /^$range$/);
+ok("06" !~ /^$range$/);
+ok("006" !~ /^$range$/);
+ok(7 =~ /^$range$/);
+ok("07" =~ /^$range$/);
+ok("007" =~ /^$range$/);
+ok(8 =~ /^$range$/);
+ok("08" =~ /^$range$/);
+ok("008" =~ /^$range$/);
+ok(60 =~ /^$range$/);
+ok("060" =~ /^$range$/);
+ok(600 !~ /^$range$/);
+ok("0600" !~ /^$range$/);
+ok("0700" !~ /^$range$/);
+ok("0800" !~ /^$range$/);
+
+$range = test_range_exhaustive(7, "0128");
+ok(128 =~ /^$range$/);
+ok("0128" =~ /^$range$/);
+ok(129 !~ /^$range$/);
+ok("0129" !~ /^$range$/);
+ok(130 !~ /^$range$/);
+ok("0130" !~ /^$range$/);
+
+sub test_range_random {
+  my($min, $max, $trials) = @_;
+  my $range = regex_range($min, $max);
+  ok($range);
+  my $spread = $max - $min;
+  ok(($min-1) !~ /^$range$/);
+  for(my $trial=0; $trial<$trials; $trial++) {
+    my $c = $min + int rand $spread * 1.5;
+    my $desired = ($c >= $min) && ($c <= $max);
+    my $actual  = "$c" =~ /^$range$/;
+    if( ($desired and $actual) or (!$desired && !$actual) ) {
+      ok(1);
+    } else {
+      warn "failed (random) test $c =~ /^$range$/\n";
+    }
+  }
+  ok(($max+1) !~ /^$range$/);
+  return $range;
+}
+
+sub test_range_partial {
+  my($min, $max, @tranges) = @_;
+  my $range = regex_range($min, $max);
+  ok($range);
+  foreach my $test (@tranges) { 
+    my ($tmin, $tmax) = ($test->[0], $test->[1]);
+    for(my $c=$tmin; $c<=$tmax; ++$c) {
+      my $desired = ($c >= $min) && ($c <= $max);
+      my $actual  = "$c" =~ /^$range$/;
+      if( ($desired and $actual) or (!$desired && !$actual) ) {
+        ok(1);
+      } else {
+        warn "failed (partial range) test $c =~ /^$range$/, min: $min, max: $max\n";
+      }
+    }
+  }
+  return $range;
+}
+
+sub test_range_exhaustive {
+  my($min, $max) = @_;
+  my $range = regex_range($min, $max);
+  ok($range);
+  ok(($min-1) !~ /^$range$/);
+  for(my $c=$min; $c<=$max; ++$c) {
+    if("$c" =~ /^$range$/) {
+      ok(1);
+    } else {
+      warn "failed (exhaustive) test $c =~ /^$range$/, min: $min, max: $max\n";
+    }
+  }
+  ok(($max+1) !~ /^$range$/);
+  return $range;
+}
+
